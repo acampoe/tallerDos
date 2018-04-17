7efc9a2b027adb1c274bc4e3288704bd54808b4a
AndresCampo <andrescampoe@gmail.com>
2018-04-16 21:13:39 -0500

    reporte cuatro terminado

--- app/src/main/java/com/movil/clase/tallerdos/ReporteCuatro.java
+++ app/src/main/java/com/movil/clase/tallerdos/ReporteCuatro.java
@@ -2,12 +2,29 @@ package com.movil.clase.tallerdos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class ReporteCuatro extends AppCompatActivity {
-
+    private ArrayList<Smartphone> telefonos;
+    private ArrayList<Smartphone> telefonos2 = new ArrayList<>();
+    private String apple = "Apple";
+    private String negro = "Negro";
+    private EditText numero;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte_cuatro);
+        telefonos = Datos.obtener();
+        numero = findViewById(R.id.txtNumero);
+        Smartphone actual;
+        for (int i = 0; i < telefonos.size(); i++) {
+            actual = telefonos.get(i);
+            if (actual.getMarca().equals(apple) && actual.getColor().equals(negro)){
+                telefonos2.add(actual);
+            }
+        }
+        numero.setText(telefonos2.size()+"");
     }
 }
